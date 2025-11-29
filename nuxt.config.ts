a129063c728a8ddedc2cc7ea6fc9e970ad165bfe
fady <fady85320@gamil.com>
2025-11-29 13:01:45 +0200

    initial commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,26 @@
 import { defineNuxtConfig } from "nuxt/config";
 
 export default defineNuxtConfig({
-   target: 'static', 
-  ssr: false,       
+  target: 'static',
+  ssr: false,
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
+
+  app: {
+    baseURL: '/NUXTFLIX/',  
+    buildAssetsDir: 'assets'
+  },
+
   modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+
   css: ["@fortawesome/fontawesome-free/css/all.min.css"],
+
   runtimeConfig: {
     public: {
       TMDB_API_KEY: process.env.TMDB_API_KEY,
     },
   },
+
   devServer: {
     websocket: {
       port: 24679,
